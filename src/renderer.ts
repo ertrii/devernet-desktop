d7622214df42893989c267fefb9971cfddc90af2
ertrii <ertriilive@gmail.com>
2021-06-29 18:40:34 -0500

    :tada: first commit

--- src/renderer.ts
+++ src/renderer.ts
@@ -4,3 +4,8 @@
 // nodeIntegration is set to true in webPreferences.
 // Use preload.js to selectively enable features
 // needed in the renderer process.
+const h1 = document.getElementById('title')
+
+h1.onclick = ev => {
+    console.log(ev)
+}
